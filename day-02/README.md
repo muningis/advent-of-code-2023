55af5632718cc2047b1aadfbc3b8f6723fdc84be
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2023-12-02 12:54:25 +0200

    🎄 Day Two - Cleanup

--- day-02/README.md
+++ day-02/README.md
@@ -3,4 +3,5 @@
 ## How to run
 
 1. Create `input.txt` file with your input (or name of your choice)
-2. Run it `cargo run -- ./input.txt`
+2. Configure `MAX_RED`, `MAX_GREEN`, `MAX_BLUE` in `src/main.rs` 
+3. Run it `cargo run -- ./input.txt`
--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,98 +1,48 @@
-use regex::Regex;
+use regex::{Regex, Captures};
 use std::{env, fs};
 
-const RED: i64 = 12;
-const GREEN: i64 = 13;
-const BLUE: i64 = 14;
+const MAX_RED: i64 = 12;
+const MAX_GREEN: i64 = 13;
+const MAX_BLUE: i64 = 14;
 
-fn is_valid(game: &str) -> bool {
-    let game_re = Regex::new(
-        r"((?<red>\d{1,} red(, )?)|(?<blue>\d{1,} blue(, )?)|(?<green>\d{1,} green(, )?))*",
-    )
-    .unwrap();
-    let digit_re = Regex::new(r"\d{1,}").unwrap();
-    let mut check = true;
-    game.split("; ").for_each(|round| {
-        let rolls = game_re.captures(round).unwrap();
-        let red = match rolls.name("red") {
-            None => 0,
-            Some(red) => digit_re
-                .find(red.as_str())
-                .unwrap()
-                .as_str()
-                .parse::<i64>()
-                .unwrap(),
-        };
-        let green = match rolls.name("green") {
-            None => 0,
-            Some(green) => digit_re
-                .find(green.as_str())
-                .unwrap()
-                .as_str()
-                .parse::<i64>()
-                .unwrap(),
-        };
-        let blue = match rolls.name("blue") {
-            None => 0,
-            Some(blue) => digit_re
-                .find(blue.as_str())
-                .unwrap()
-                .as_str()
-                .parse::<i64>()
-                .unwrap(),
-        };
-        if red > RED || green > GREEN || blue > BLUE {
-            check = false;
-        }
-    });
-    check
+fn capture_into_i64(capture: &Captures<'_>, name: &str) -> i64 {
+  let digit_re = Regex::new(r"\d{1,}").unwrap();
+
+  match capture.name(name) {
+    None => 0,
+    Some(red) => digit_re
+        .find(red.as_str())
+        .unwrap()
+        .as_str()
+        .parse::<i64>()
+        .unwrap(),
+  }
 }
 
-fn get_power(game: &str) -> i64 {
+fn iterate_game(game: &str) -> (bool, i64) {
     let game_re = Regex::new(
         r"((?<red>\d{1,} red(, )?)|(?<blue>\d{1,} blue(, )?)|(?<green>\d{1,} green(, )?))*",
     )
     .unwrap();
-    let digit_re = Regex::new(r"\d{1,}").unwrap();
+
+    let mut check = true;
     let mut min_red: i64 = 1;
     let mut min_green: i64 = 1;
     let mut min_blue: i64 = 1;
 
     game.split("; ").for_each(|round| {
-      let rolls = game_re.captures(round).unwrap();
-      let red = match rolls.name("red") {
-          None => 0,
-          Some(red) => digit_re
-              .find(red.as_str())
-              .unwrap()
-              .as_str()
-              .parse::<i64>()
-              .unwrap(),
-      };
-      let green = match rolls.name("green") {
-          None => 0,
-          Some(green) => digit_re
-              .find(green.as_str())
-              .unwrap()
-              .as_str()
-              .parse::<i64>()
-              .unwrap(),
-      };
-      let blue = match rolls.name("blue") {
-          None => 0,
-          Some(blue) => digit_re
-              .find(blue.as_str())
-              .unwrap()
-              .as_str()
-              .parse::<i64>()
-              .unwrap(),
-      };
-      if red > min_red { min_red = red }
-      if blue > min_blue { min_blue = blue }
-      if green > min_green { min_green = green }
-  });
-
-    min_blue * min_green * min_red
+        let rolls = game_re.captures(round).unwrap();
+        let red = capture_into_i64(&rolls, "red");
+        let green = capture_into_i64(&rolls, "green");
+        let blue = capture_into_i64(&rolls, "blue");
+        if red > MAX_RED || green > MAX_GREEN || blue > MAX_BLUE {
+            check = false;
+        }
+        if red > min_red { min_red = red }
+        if blue > min_blue { min_blue = blue }
+        if green > min_green { min_green = green }
+    });
+    (check, min_blue * min_green * min_red)
 }
 
 fn main() {
@@ -114,8 +64,9 @@ fn main() {
     captures.for_each(|capture| {
         let game_id = capture.name("game_id").unwrap().as_str();
         let game = capture.get(2).unwrap().as_str();
-        powers.push(get_power(game));
-        if is_valid(game) {
+        let (is_valid, power) = iterate_game(game);
+        powers.push(power);
+        if is_valid {
             valid_ids.push(game_id.parse::<i64>().unwrap());
         }
     });
