414fc33ee62c61ffe09affad9071a2bd9244b5f6
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2023-12-05 20:57:02 +0200

    🎄 Day Five - refactor tests and utils to work with ranges instead

--- src/day/five.rs
+++ src/day/five.rs
@@ -1,9 +1,5 @@
 use regex::Regex;
-use std::{
-  collections::{HashMap, HashSet},
-  env, fs,
-  io::Error,
-};
+use std::{collections::HashSet, env, fs, io::Error};
 
 pub fn day_five() -> Result<(), Error> {
   let args: Vec<String> = env::args().collect();
@@ -16,42 +12,42 @@ pub fn day_five() -> Result<(), Error> {
 
   let mut lines = contents.lines().into_iter();
 
-  let mut active = parse_seeds_numbers(&lines.next().unwrap());
+  let mut active = parse_seeds_ranges(&lines.next().unwrap());
 
-  for line in lines {
-    if line.ends_with("map:") {
-      println!("Previous active is {:?}", active);
-      active = active
-        .clone()
-        .into_iter()
-        .map(|(_, value)| (value, value))
-        .collect();
-      println!("New active is {:?}", active);
-      continue;
-    }
+  // for line in lines {
+  //   if line.ends_with("map:") {
+  //     println!("Previous active is {:?}", active);
+  //     active = active
+  //       .clone()
+  //       .into_iter()
+  //       .map(|(_, value)| (value, value))
+  //       .collect();
+  //     println!("New active is {:?}", active);
+  //     continue;
+  //   }
 
-    if line.is_empty() {
-      continue;
-    }
+  //   if line.is_empty() {
+  //     continue;
+  //   }
 
-    let (destination, source, length) = parse_digits_in_line(&line);
-    active.clone().keys().into_iter().for_each(|key| {
-      if key >= &source && key <= &(source + (length - 1)) {
-        let offset = key - source;
-        *active.get_mut(key).unwrap() = destination + offset;
-      }
-    });
-  }
-  let min = active.values().into_iter().min().unwrap();
-  println!(
-    "lowest location number that corresponds to any of the initial seed numbers: {:?}",
-    min
-  );
+  //   let (destination, source, length) = parse_digits_in_line(&line);
+  //   active.clone().keys().into_iter().for_each(|key| {
+  //     if key >= &source && key <= &(source + (length - 1)) {
+  //       let offset = key - source;
+  //       *active.get_mut(key).unwrap() = destination + offset;
+  //     }
+  //   });
+  // }
+  // let min = active.values().into_iter().min().unwrap();
+  // println!(
+  //   "lowest location number that corresponds to any of the initial seed numbers: {:?}",
+  //   min
+  // );
 
   Ok(())
 }
 
-fn parse_digits_in_line(line: &str) -> (usize, usize, usize) {
+fn parse_location_ranges(line: &str) -> ((usize, usize), (usize,usize)) {
   let re = Regex::new(r"(?<destination>\d+)\s(?<source>\d+)\s(?<length>\d+)").unwrap();
   let captures = re.captures(line).unwrap();
   let source = captures
@@ -73,21 +69,31 @@ fn parse_digits_in_line(line: &str) -> (usize, usize, usize) {
     .parse::<usize>()
     .unwrap();
 
-  (destination, source, length)
+  ((destination, destination + length - 1), (source, source + length - 1))
 }
 
-fn parse_seeds_numbers(line: &str) -> HashMap<usize, usize> {
+fn parse_seeds_ranges(line: &str) -> HashSet<(usize, usize)> {
   let re = Regex::new(r"(\d+ \d+)").unwrap();
-  let mut seeds: HashMap<usize, usize> = HashMap::new();
+  let mut seeds_ranges: HashSet<(usize, usize)> = HashSet::new();
   re.find_iter(line).for_each(|m| {
     let mut start_and_length = m.as_str().split(" ").into_iter();
-    let start: usize = start_and_length.next().unwrap().parse::<usize>().unwrap().to_owned();
-    let length: usize = start_and_length.next().unwrap().parse::<usize>().unwrap().to_owned();
-    for i in start..start+length {
-      seeds.insert(i, i);
-    }
+    let start: usize = start_and_length
+      .next()
+      .unwrap()
+      .parse::<usize>()
+      .unwrap()
+      .to_owned();
+    let end: usize = start
+      + start_and_length
+        .next()
+        .unwrap()
+        .parse::<usize>()
+        .unwrap()
+        .to_owned()
+      - 1;
+    seeds_ranges.insert((start, end));
   });
-  seeds
+  seeds_ranges
 }
 
 #[cfg(test)]
@@ -96,17 +102,14 @@ mod tests {
 
   #[test]
   fn line_is_parsed() {
-    let result = parse_digits_in_line("13 37 9001");
-    assert_eq!(result, (13, 37, 9001));
+    let result = parse_location_ranges("13 37 9001");
+    assert_eq!(result, ((13, 9013), (37, 9037)));
   }
 
   #[test]
-  fn get_seeds() {
-    let result = parse_seeds_numbers("seeds: 12 3 23 4");
-    let expected: HashMap<usize, usize> = [(12, 12), (13, 13), (14,14), (23, 23), (24, 24), (25, 25), (26, 26)]
-      .iter()
-      .cloned()
-      .collect();
+  fn get_seeds_ranges() {
+    let result = parse_seeds_ranges("seeds: 12 200 23 430");
+    let expected: HashSet<(usize, usize)> = [(12, 211), (23, 452)].iter().cloned().collect();
     assert_eq!(result, expected);
   }
 }
